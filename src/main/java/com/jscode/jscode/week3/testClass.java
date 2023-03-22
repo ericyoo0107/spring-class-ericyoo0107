3d080b94a5b10101a5b8e52b4742924f53ab3931
JunHyuk <ericyoo0107@gmail.com>
2023-03-22 22:30:14 +0900

    feat: 반환하는 json을 로그로 찍도록 하고 로그 레벨 설정을 info로 정함

--- src/main/java/com/jscode/jscode/week3/testClass.java
+++ src/main/java/com/jscode/jscode/week3/testClass.java
@@ -1,16 +1,19 @@
 package com.jscode.jscode.week3;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Slf4j
 public class testClass {
     @GetMapping("/api/user")
     public User returnUser()
     {
         User user = new User("Junhyuk", 20, "man");
+        log.info(String.valueOf(user));
         return user;
     }
 
